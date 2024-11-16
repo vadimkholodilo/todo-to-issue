94025a14cf97dc561715cfec4596a2cfc2eb50ba
vadimkholodilo <vadimkholodilo@gmail.com>
2024-11-16 05:34:08 +0300

    Add start code

---
+++ src/utils.js
@@ -0,0 +1,27 @@
+﻿const fs = require('fs');
+const path = require('path');
+
+module.exports.findCsFiles = (startPath, maxDepth, currentDepth = 0) => {
+    const results = [];
+
+    if (currentDepth > maxDepth) {
+        return results;
+    }
+
+    const files = fs.readdirSync(startPath);
+
+    for (const file of files) {
+        const filePath = path.join(startPath, file);
+        const stat = fs.statSync(filePath);
+
+        if (stat.isDirectory()) {
+            results.push(...findCsFiles(filePath, maxDepth, currentDepth + 1));
+        }
+
+        if (path.extname(file) === '.cs') {
+            results.push(filePath);
+        }
+    }
+
+    return results;
+};
