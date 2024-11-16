4e320f9f34728592cffc9940ce54db348d976bfa
vadimkholodilo <vadimkholodilo@gmail.com>
2024-11-16 06:40:34 +0300

    Refactor and reformat code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
-﻿
+﻿const utils = require("./utils");
+
--- src/utils.js
+++ src/utils.js
@@ -1,27 +1,31 @@
 ﻿const fs = require('fs');
 const path = require('path');
 
-module.exports.findCsFiles = (startPath, maxDepth, currentDepth = 0) => {
+/**
+ * Finds files with the specified extensions.
+ * @param {string} startPath
+ * @param {string[]} extensions
+ * @param {number} maxDepth
+ * @param {number} currentDepth
+ * @returns {string[]}
+ */
+function findFilesWithExtensions(startPath, extensions, maxDepth, currentDepth = 0) {
     const results = [];
 
-    if (currentDepth > maxDepth) {
+    if (currentDepth > maxDepth)
         return results;
-    }
 
     const files = fs.readdirSync(startPath);
-
     for (const file of files) {
         const filePath = path.join(startPath, file);
         const stat = fs.statSync(filePath);
-
-        if (stat.isDirectory()) {
-            results.push(...findCsFiles(filePath, maxDepth, currentDepth + 1));
-        }
-
-        if (path.extname(file) === '.cs') {
+        if (stat.isDirectory())
+            results.push(...findFilesWithExtensions(filePath, extensions, maxDepth, currentDepth + 1));
+        if (extensions.includes(path.extname(file)))
             results.push(filePath);
-        }
     }
 
     return results;
-};
+}
+
+module.exports.findFilesWithExtensions = findFilesWithExtensions;
